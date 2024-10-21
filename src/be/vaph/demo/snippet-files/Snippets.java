e200fc612f1c3e689b5e53950602116a51ea0b60
Jordy Cantaert <jordy.cantaert@vaph.be>
2024-10-21 09:29:01 +0200

    typos

--- src/be/vaph/demo/snippet-files/Snippets.java
+++ src/be/vaph/demo/snippet-files/Snippets.java
@@ -2,13 +2,13 @@ import java.util.Optional;
 
 class Snippets {
 
-    // @start region="example"
+    // @start region = "example"
     private String snippet;
 
     public Snippets(String snippet) {}
     // @end
 
-    // @start region = other-example
+    // @start region = "other-example"
     public Optional<String> getSnippet() {
         return Optional.of(snippet);
     }
